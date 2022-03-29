960bdc0ef3f684c9c7b1ea43180bcbbba302daf8
M. Wierzbicki <michalwierzbicki.miw@gmail.com>
2022-03-29 10:39:42 +0200

    add logic and basic style, without spinner or progress bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import './App.css';
+import { useState } from 'react';
 
-const checkNumber = (evt) => {
-  if(evt.key === 'Enter'){
-  return console.log(evt.target.value)}
-}
+
 
 function App() {
+
+  const [value, setValue] = useState();
+  const [userAnswerTable, setUserAnswerTable] = useState([])
+
+  const min = 0
+  const max = 10000000
+
+  const checkNumber = (evt) => {
+    let tableDivisibleNumber = []
+    if (evt.key === 'Enter') {
+      for (let i = 1; i <= evt.target.value; i++) {
+        if (i % 3 === 0) {
+          tableDivisibleNumber.push(i)
+        }
+      }
+      evt.target.value = '';
+      return setUserAnswerTable(tableDivisibleNumber)
+    }
+  }
+
   return (
     <div className="App">
-      <input type='number' placeholder="Wpisz liczbę i naciśnij Enter!" onKeyPress={checkNumber.bind(this)}/>
+      <h1 className='title'>Wpisz liczbę od 0 do 10 000 000</h1>
+      <input
+      className='input'
+        type='number'
+        placeholder="Wpisz liczbę i naciśnij Enter!"
+        onKeyPress={checkNumber.bind(this)}
+        value={value}
+        onChange={(e) => {
+          const value = parseInt(e.target.value, 10);
+
+          if (value > max) value = max;
+          if (value < min) value = min;
+
+          setValue(value);
+        }} />
+      <div className='user_answer'>
+          {userAnswerTable.map((value) => (
+            <p>{value}</p>
+          ))}
+      </div>
     </div>
   );
 }
